d07b2f4f70f8e9a570573a3bc352cf59d192c995
8Andre8 <boing1999@mail.ru>
2020-09-30 13:08:34 +0300

    finished

--- ЛР1 - синтаксис/ООП ЛР1 - синтаксис.cpp
+++ ЛР1 - синтаксис/ООП ЛР1 - синтаксис.cpp
@@ -1,17 +1,23 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <stdio.h>
+
 using namespace std;
 
-void arithmetic_expression();
-void logical_expression();
-void range();
-void array_work();
-void print_array(double** array, int n, int m);
-void print_array_to_file(double** array, int n, int m, char filename[]);
-void read_array_from_file(double** array, int n, int m, char filename[]);
-double* count_products_of_negatives(double** array, int n, int m);
-void find_minimal_in_row(double** array, int n, int m);
-double* create_array(int n);
+void arithmetic_expression(); // Вычисление арифметического выражения
+void logical_expression();    // Вычисление логического выражения
+void range();                 // Вычсиление значения функции с помощью ряда
+void array_work();            // Работа с массивом
+void print_array(double** array, int n, int m); // Печать массива в консоль
+void print_array_to_file(double** array, int n, int m, char filename[]); // Печать массива в файл
+void read_array_from_file(double** array, int n, int m, char filename[]); // Чтение массива из файла
+double* count_products_of_negatives(double** array, int n, int m); // Подсчет произведений 
+// отрицательных элементов по каждому столбцу массива
+void find_minimal_in_row(double** array, int n, int m); // Нахождение минимального элемента в строке
+double** create_bivariate_array(int n, int m); // Создание двумерного динамического массива
+double* create_array(int n); // Создание одномерного динамического массива
+void delete_bivariate_array(double** bivariate_array, int n); // Удаление двумерного динамического массива
+void delete_array(double* array); // Удаление одномерного динамического массива
 
 int main()
 {
@@ -21,11 +27,11 @@ int main()
 	while (work)
 	{
 		cout << "Что будем делать?\n"
-			<< "1 - Вычисление арифметического выражения\n"
-			<< "2 - Вычисление логического выражения\n"
-			<< "3 - Вычисление значения функции с помощью ряда\n"
-			<< "4 - Работа с массивом\n"
-			<< "Любой другой символ - выход\n";
+			 << "1 - Вычисление арифметического выражения\n"
+			 << "2 - Вычисление логического выражения\n"
+			 << "3 - Вычисление значения функции с помощью ряда\n"
+			 << "4 - Работа с массивом\n"
+			 << "Любой другой символ - выход\n";
 		cin >> choice;
 		system("cls");
 		switch (choice)
@@ -52,6 +58,7 @@ int main()
 void arithmetic_expression()
 {
 	double x, y, z;
+	const int one = 1;
 	cout << "Арифметическое выражение:\n";
 	cout << "R = X * Y * (X - Z) / ((Y - X) * (1 + (Z - Y))) + fabs(exp(X * Y) - sqrt(Z / Y)): \n";
 	cout << "Введите X\n";
@@ -60,12 +67,13 @@ void arithmetic_expression()
 	cin >> y;
 	cout << "Введите Z\n";
 	cin >> z;
-	cout << "R = " << x * y * (x - z) / ((y - x) * (1 + (z - y))) + fabs(exp(x * y) - sqrt(z / y)) << "\n\n";
+	cout << "R = " << x * y * (x - z) / ((y - x) * (1 + (z - y))) + fabs(exp(x * y) - sqrt(z / y)) * one << "\n\n";
 }
 
 void logical_expression()
 {
 	int a, b, c;
+	const int zero = 0;
 	cout << "Логическое выражение:\n";
 	cout << "(A && B && C) || (!A && !B && !C)\n";
 	cout << "Введите A\n";
@@ -74,9 +82,9 @@ void logical_expression()
 	cin >> b;
 	cout << "Введите C\n";
 	cin >> c;
-	a = a != 0;
-	b = b != 0;
-	c = c != 0;
+	a = a != zero;
+	b = b != zero;
+	c = c != zero;
 	cout << ((a && b && c) || (!a && !b && !c) ? "Результат: True\n\n" : "Результат: False\n\n");
 }
 
@@ -116,32 +124,30 @@ void array_work()
 	cin >> n;
 	cout << "Введите количество строк m (не больше 5)\n";
 	cin >> m;
-	double** array = new double* [n];
-	for (int i = 0; i < n; i++)
-	{
-		array[i] = new double[m];
-	}
+	// Создание двумерного динамического массива
+	double** array = create_bivariate_array(n, m);
+	// Исходный файл
 	char filename[] = "array.txt";
+	// Чтение массива из файла
 	read_array_from_file(array, n, m, filename);
 	cout << "\nИсходный массив:\n";
+	// Печать массива в консоль
 	print_array(array, n, m);
-	//
+	// Файл для вывода 
 	char filename_out[] = "array_out_original.txt";
+	// Печать массива в файл
 	print_array_to_file(array, n, m, filename_out);
-	//
+	// Подсчет произведений отрицательных элементов по каждому столбцу массива
 	double* array_of_products = count_products_of_negatives(array, n, m);
 	for (int j = 0; j < m; j++)
 	{
-		//cout << array_of_products[j] << ' ';
 		printf("%8.3f ", array_of_products[j]);
 	}
+	// Нахождение минимального элемента в каждой строке массива
 	find_minimal_in_row(array, n, m);
-	for (int i = 0; i < n; i++)
-	{
-		delete[] array[i];
-	}
-	delete[] array;
-	delete[] array_of_products;
+	// Освобождение выделенной памяти
+	delete_bivariate_array(array, n);
+	delete_array(array_of_products);
 }
 	
 void print_array(double** array, int n, int m)
@@ -150,7 +156,6 @@ void print_array(double** array, int n, int m)
 	{
 		for (int j = 0; j < m; j++)
 		{
-			//cout << array[i][j] << ' ';
 			printf("%8.3f ", array[i][j]);
 		}
 		cout << endl;
@@ -159,16 +164,24 @@ void print_array(double** array, int n, int m)
 
 void print_array_to_file(double** array, int n, int m, char filename[])
 {
-	ofstream output_file(filename);
-	for (int i = 0; i < n; i++)
+	FILE* output_file;
+	errno_t err;
+	if ((err = fopen_s(&output_file, filename, "w")) != 0)
 	{
-		for (int j = 0; j < m; j++)
-		{
-			output_file << array[i][j] << ' ';
-		}
-		output_file << endl;
+		cout << "Ошибка! Файл не был открыт.";
 	}
-	output_file.close();
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				fprintf(output_file, "%8.3f ", array[i][j]);
+			}
+			fprintf(output_file, "\n");
+		}
+	}
+	fclose(output_file);
 }
 
 void read_array_from_file(double** array, int n, int m, char filename[])
@@ -228,7 +241,6 @@ void find_minimal_in_row(double** array, int n, int m)
 				min_j = j;
 			}
 		}
-		//cout << min << ' ' << i << ' ' << min_j << '\n';
 		printf("%8.3f %8d %8d", min, i, min_j);
 		cout << endl;
 		min = 1e6;
@@ -237,8 +249,30 @@ void find_minimal_in_row(double** array, int n, int m)
 	cout << endl;
 }
 
+double** create_bivariate_array(int n, int m){
+	double** bivariate_array = new double* [n];
+	for (int i = 0; i < n; i++)
+	{
+		bivariate_array[i] = new double[m];
+	}
+	return bivariate_array;
+}
+
 double* create_array(int n)
 {
 	double* array = new double[n];
 	return array;
+}
+
+void delete_bivariate_array(double** bivariate_array, int n)
+{
+	for (int i = 0; i < n; i++) {
+		delete[] bivariate_array[i];
+	}
+	delete[] bivariate_array;
+}
+
+void delete_array(double* array) 
+{
+	delete[] array;
 }
