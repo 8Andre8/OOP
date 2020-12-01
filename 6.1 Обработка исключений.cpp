afae724020cce3134ed149e4b41c321eaa346238
Andreev Maxim <49001876+8Andre8@users.noreply.github.com>
2020-12-01 23:39:55 +0300

    Add files via upload

---
+++ 6.1 Обработка исключений.cpp
@@ -0,0 +1,308 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+using namespace std;
+
+void arithmetic_expression(); // Вычисление арифметического выражения
+void logical_expression();    // Вычисление логического выражения
+void range();                 // Вычсиление значения функции с помощью ряда
+void array_work();            // Работа с массивом
+void print_array(double** array, int n, int m); // Печать массива в консоль
+void print_array_to_file(double** array, int n, int m, char filename[]); // Печать массива в файл
+void read_array_from_file(double** array, int n, int m, char filename[]); // Чтение массива из файла
+double* count_products_of_negatives(double** array, int n, int m); // Подсчет произведений 
+// отрицательных элементов по каждому столбцу массива
+void find_minimal_in_row(double** array, int n, int m); // Нахождение минимального элемента в строке
+double** create_bivariate_array(int n, int m); // Создание двумерного динамического массива
+double* create_array(int n); // Создание одномерного динамического массива
+void delete_bivariate_array(double** bivariate_array, int n); // Удаление двумерного динамического массива
+void delete_array(double* array); // Удаление одномерного динамического массива
+
+
+int main()
+{
+	char choice;
+	int work = 1;
+	setlocale(LC_ALL, "Russian");
+	while (work)
+	{
+		cout << "Что будем делать?\n"
+			<< "1 - Вычисление арифметического выражения\n"
+			<< "2 - Вычисление логического выражения\n"
+			<< "3 - Вычисление значения функции с помощью ряда\n"
+			<< "4 - Работа с массивом\n"
+			<< "Любой другой символ - выход\n";
+		cin >> choice;
+		system("cls");
+		switch (choice)
+		{
+		case '1':
+			arithmetic_expression();
+			break;
+		case '2':
+			logical_expression();
+			break;
+		case '3':
+			range();
+			break;
+		case '4':
+			array_work();
+			break;
+		default:
+			work = 0;
+			break;
+		}
+	}
+}
+
+void arithmetic_expression()
+{
+	double x, y, z;
+	cout << "Арифметическое выражение:\n";
+	cout << "R = X * Y * (X - Z) / ((Y - X) * (1 + (Z - Y))) + fabs(exp(X * Y) - sqrt(Z / Y)): \n";
+	cout << "Введите X\n";
+	cin >> x;
+	cout << "Введите Y\n";
+	cin >> y;
+	cout << "Введите Z\n";
+	cin >> z;
+	cout << "R = " << x * y * (x - z) / ((y - x) * (1 + (z - y))) + fabs(exp(x * y) - sqrt(z / y)) << "\n\n";
+}
+
+void logical_expression()
+{
+	int a, b, c;
+	cout << "Логическое выражение:\n";
+	cout << "(A && B && C) || (!A && !B && !C)\n";
+	cout << "Введите A\n";
+	cin >> a;
+	cout << "Введите B\n";
+	cin >> b;
+	cout << "Введите C\n";
+	cin >> c;
+	a = a != 0;
+	b = b != 0;
+	c = c != 0;
+	cout << ((a && b && c) || (!a && !b && !c) ? "Результат: True\n\n" : "Результат: False\n\n");
+}
+
+void range()
+{
+	double x;
+	double eps = 1e-6;
+	double f = 0;
+	double f_n = 1e+6;
+	cout << "arctg(1/x) = 1 / x - 1/3x^3 + 1/5x^5 - 1/7x^7 + ... \n";
+	cout << "Область сходимости ряда (-inf, -1] U [1, + inf)\n";
+	cout << "Введите x\n";
+	cin >> x;
+	if (x == 0)
+	{
+		cout << "Деление на 0!\n\n";
+		return;
+	}
+	if (fabs(x) < 1)
+	{
+		cout << "Ряд расходится!\n\n";
+		return;
+	}
+	for (int n = 0; fabs(f_n) > eps; n++)
+	{
+		f_n = pow(-1, n) * 1 / ((2 * (double)n + 1) * pow(x, 2 * n + 1));
+		f += f_n;
+	}
+	cout << "arctg(1/x) = " << f << "\n";
+	cout << "Проверка: arctg(1/x) = " << atan(1 / x) << "\n\n";
+}
+
+void array_work()
+{
+	int n = 0, m = 0;
+	int DO = 1;
+	double** array = NULL;
+	// Исходный файл
+	char filename[] = "array.txt";
+	while (DO)
+	{
+		try
+		{
+			cout << "Введите количество строк n (не больше 4)\n";
+			cin >> n;
+			cout << "Введите количество строк m (не больше 5)\n";
+			cin >> m;
+			// Создание двумерного динамического массива
+			array = create_bivariate_array(n, m);
+			// Чтение массива из файла
+			read_array_from_file(array, n, m, filename);
+		}
+		catch (int i)
+		{
+			cout << "Вызвано исключение: неожиданный конец файла\n\n";
+			continue;
+		}
+		DO = 0;
+	}
+	cout << "\nИсходный массив:\n";
+	// Печать массива в консоль
+	print_array(array, n, m);
+	// Файл для вывода 
+	char filename_out[] = "array_out_original.txt";
+	// Печать массива в файл
+	print_array_to_file(array, n, m, filename_out);
+	// Подсчет произведений отрицательных элементов по каждому столбцу массива
+	double* array_of_products = count_products_of_negatives(array, n, m);
+	for (int j = 0; j < m; j++)
+	{
+		printf("%8.3f ", array_of_products[j]);
+	}
+	// Нахождение минимального элемента в каждой строке массива
+	find_minimal_in_row(array, n, m);
+	// Освобождение выделенной памяти
+	delete_bivariate_array(array, n);
+	delete_array(array_of_products);
+}
+
+void print_array(double** array, int n, int m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			printf("%8.3f ", array[i][j]);
+		}
+		cout << endl;
+	}
+}
+
+void print_array_to_file(double** array, int n, int m, char filename[])
+{
+	FILE* output_file;
+	errno_t err;
+	if ((err = fopen_s(&output_file, filename, "w")) != 0)
+	{
+		cout << "Ошибка! Файл не был открыт.";
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				fprintf(output_file, "%8.3f ", array[i][j]);
+			}
+			fprintf(output_file, "\n");
+		}
+	}
+	fclose(output_file);
+}
+
+void read_array_from_file(double** array, int n, int m, char filename[])
+{
+	ifstream input_file(filename);
+	// Обработка исключений
+	try {
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				// Если встречен конец файла, то вызывается исключение
+				if (input_file.eof())
+				{
+					throw - 1;
+				}
+				input_file >> array[i][j];
+			}
+		}
+	}
+	catch (int i)
+	{
+		throw - 1;
+		input_file.close();
+	}
+	input_file.close();
+}
+
+double* count_products_of_negatives(double** array, int n, int m)
+{
+	double* array_of_products = create_array(m);
+	int k = 0;
+	double mul = 1;
+	bool have_negative = false;
+	cout << "\nМассив из произведений отрицательных элементов каждого столбца:\n";
+	for (int j = 0; j < m; j++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (array[i][j] < 0)
+			{
+				mul *= array[i][j];
+				have_negative = true;
+			}
+		}
+		if (!have_negative)
+		{
+			mul = 0;
+		}
+		array_of_products[k] = mul;
+		mul = 1;
+		have_negative = false;
+		k++;
+	}
+	return array_of_products;
+}
+
+void find_minimal_in_row(double** array, int n, int m)
+{
+	double min = 1e6;
+	int min_j = 0;
+	cout << "\n\nМинимальные элементы по каждой строке и их индексы:\n";
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (array[i][j] < min)
+			{
+				min = array[i][j];
+				min_j = j;
+			}
+		}
+		printf("%8.3f %8d %8d", min, i, min_j);
+		cout << endl;
+		min = 1e6;
+		min_j = 0;
+	}
+	cout << endl;
+}
+
+double** create_bivariate_array(int n, int m) {
+	double** bivariate_array = new double* [n];
+	for (int i = 0; i < n; i++)
+	{
+		bivariate_array[i] = new double[m];
+		for (int j = 0; j < m; j++)
+		{
+			bivariate_array[i][j] = 0;
+		}
+	}
+	return bivariate_array;
+}
+
+double* create_array(int n)
+{
+	double* array = new double[n];
+	return array;
+}
+
+void delete_bivariate_array(double** bivariate_array, int n)
+{
+	for (int i = 0; i < n; i++) {
+		delete[] bivariate_array[i];
+	}
+	delete[] bivariate_array;
+}
+
+void delete_array(double* array)
+{
+	delete[] array;
+}
---
+++ 6.2 Шаблоны.cpp
@@ -0,0 +1,446 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <list>
+
+using namespace std;
+
+// Шаблонный класс списка
+template <typename T>
+class List
+{
+private:
+	struct list_ob
+	{
+		T _value;
+		list_ob* _prev = NULL;
+		list_ob* _next = NULL;
+	};
+	int _size;
+	list_ob* _head;
+public:
+	// Конструктор1 списка 
+	List()
+	{
+		_size = 0;
+		_head = NULL;
+	}
+	// Конструктор2 списка (элемент в параметре сразу заносится в список)
+	List(T x)
+	{
+		_size = 1;
+		_head = new(list_ob);
+		_head->_value = x;
+		_head->_prev = NULL;
+		_head->_next = NULL;
+	}
+
+	~List() {}
+
+	// Добавить элемент в список
+	void add_to_list(T x)
+	{
+		if (_size == 0)
+		{
+			_size = 1;
+			_head = new(list_ob);
+			_head->_value = x;
+			_head->_prev = NULL;
+			_head->_next = NULL;
+			return;
+		}
+		_size++;
+		list_ob* cur = new(list_ob);
+		if (_head->_next == NULL)
+		{
+			if (x < _head->_value)
+			{
+				cur->_prev = NULL;
+				cur->_next = _head;
+				_head->_prev = cur;
+				_head = cur;
+				_head->_value = x;
+			}
+			else
+			{
+				_head->_next = cur;
+				cur->_prev = _head;
+				cur->_next = NULL;
+				cur->_value = x;
+			}
+			return;
+		}
+		list_ob* cur_addr = cur;
+		cur = _head;
+		while (cur->_next != NULL)
+		{
+			if (x < cur->_value)
+			{
+				if (cur == _head)
+				{
+					cur = cur_addr;
+					cur->_prev = NULL;
+					cur->_next = _head;
+					cur->_value = x;
+					_head->_prev = cur;
+					_head = cur;
+					return;
+				}
+				list_ob* left, * right;
+				left = cur->_prev;
+				right = cur;
+				cur = left;
+				cur->_next = cur_addr;
+				cur = cur_addr;
+				cur->_prev = left;
+				cur->_next = right;
+				cur->_value = x;
+				cur = right;
+				cur->_prev = cur_addr;
+				return;
+			}
+			cur = cur->_next;
+		}
+		if (cur->_next == NULL)
+		{
+			if (x >= cur->_value)
+			{
+				list_ob* left;
+				left = cur;
+				cur->_next = cur_addr;
+				cur = cur_addr;
+				cur->_prev = left;
+				cur->_next = NULL;
+				cur->_value = x;
+				return;
+			}
+			else
+			{
+				list_ob* left, * last;
+				left = cur->_prev;
+				last = cur;
+				cur = left;
+				cur->_next = cur_addr;
+				cur = cur_addr;
+				cur->_prev = left;
+				cur->_next = last;
+				cur->_value = x;
+				cur = last;
+				cur->_prev = cur_addr;
+				cur->_next = NULL;
+				return;
+			}
+		}
+	}
+
+	// Перегрузка оператора += для добавления элемента списка
+	// Элемент для добавления передается в параметре
+	void operator+=(T x)
+	{
+		this->add_to_list(x);
+	}
+
+	void read_from_file(char filename[])
+	{
+		ifstream fin(filename);
+		if (!fin) { cout << "Ошибка! Невозможно открыть файл.\n"; }
+		T el;
+		while (!fin.eof())
+		{
+			fin >> el;
+			this->add_to_list(el);
+		}
+		fin.close();
+	}
+
+	// Вывод головы списка
+	T peek()
+	{
+		return _head->_value;
+	}
+	// Вывод списка
+	void print_list()
+	{
+		if (_size == 0)
+		{
+			cout << "Список пуст!\n";
+			return;
+		}
+		list_ob* head;
+		head = _head;
+		while (_head != NULL)
+		{
+			cout << peek() << endl;
+			_head = _head->_next;
+		}
+		_head = head;
+	}
+	// Поиск элемента в списке
+	// Элемент для поиска передается в параметре 
+	int find_element(T x)
+	{
+		if (_size == 0)
+		{
+			cout << "Список пуст. Искать нечего\n";
+			return 0;
+		}
+		list_ob* head;
+		head = _head;
+		while (_head != NULL)
+		{
+			if (_head->_value == x)
+			{
+				cout << "Элемент \"" << x << "\" найден\n";
+				_head = head;
+				return 1;
+			}
+			_head = _head->_next;
+		}
+		cout << "Элемента \"" << x << "\" в списке нет\n";
+		_head = head;
+		return 0;
+	}
+	// Перегрузка оператора -= для удаления элемента списка
+	// Элемент для удаления передается в параметре 
+	void operator-=(T x)
+	{
+		if (_size == 0)
+		{
+			cout << "Список пуст. Удалять нечего.\n";
+			return;
+		}
+		list_ob* cur, * rem;
+		cur = _head;
+		while (cur != NULL)
+		{
+			if (cur->_value == x)
+			{
+				if (cur == _head)
+				{
+					if (_size > 1)
+					{
+						rem = _head;
+						_head = cur->_next;
+						cur = cur->_next;
+						cur->_prev = rem;
+						delete rem;
+						_size--;
+						cout << "Элемент \"" << x << "\" удален\n";
+						return;
+					}
+					else
+					{
+						delete _head;
+						_head = NULL;
+						_size--;
+						cout << "Элемент \"" << x << "\" удален\n";
+						return;
+					}
+				}
+				else
+				{
+					list_ob* left;
+					left = cur->_prev;
+					rem = cur;
+					if (cur->_next != NULL)
+					{
+						cur = left;
+						cur->_next = rem->_next;
+						cur = rem->_next;
+						cur->_prev = left;
+					}
+					else
+					{
+						left->_next = NULL;
+					}
+					delete rem;
+					_size--;
+					cout << "Элемент \"" << x << "\" удален\n";
+					return;
+				}
+			}
+			cur = cur->_next;
+		}
+		cout << "Элемента \"" << x << "\" в списке нет\n";
+	}
+	// Удаление списка целиком
+	void delete_list()
+	{
+		list_ob* cur = _head;
+		while (_head != NULL)
+		{
+			cur = _head;
+			_head = _head->_next;
+			delete cur;
+			_size--;
+		}
+	}
+	//Функция возвращает размер списка (количество элементов)
+	int stack_size()
+	{
+		return _size;
+	}
+
+ friend ostream& operator <<(ostream& os, List<T>& l)
+{
+	if (l._size == 0)
+	{
+		return os << "Список пуст!\n";
+	}
+	list_ob* head;
+	head = l._head;
+	while (l._head != NULL)
+	{
+		os << l.peek() << endl;
+		l._head = l._head->_next;
+	}
+	l._head = head;
+	return os;
+}
+}; // Конец класса списка
+
+// Создание манипулятора
+ostream& setup(ostream& stream)
+{
+	stream.setf(ios::left);
+	stream << setw(50) << setfill('=');
+	return stream;
+}
+
+// Шаблонная функция для чтения данных из файла и записи в STL-список
+template <typename T>
+void read_from_file(char filename[], list<T>& listSTL)
+{
+	ifstream fin(filename);
+	if (!fin) { cout << "Ошибка! Невозможно открыть файл.\n"; }
+	T el;
+	while (!fin.eof())
+	{
+		fin >> el;
+		listSTL.push_back(el);
+	}
+	fin.close();
+}
+
+// Шаблонная функция для вывода элементов STL-списка
+template <typename T>
+void printListSTL(const list<T>& listSTL)
+{
+	for (auto i = listSTL.cbegin(); i != listSTL.cend(); ++i) 
+	{
+		// auto - ключевое слово, позволяющее определить тип i автоматически при инициализации
+		// i - константный итератор
+		cout << *i << endl;
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char data[] = "list_data.txt";
+
+	// Работа с шаблонным списком (тип string)
+	List<string> a;
+	a.read_from_file(data); // чтение из файла
+	cout << setup << "" << endl;
+	cout << "Вывод списка (тип string)\n";
+	cout << setup << "" << endl;
+	cout << a << endl;
+	cout << setup << "" << endl;
+	cout << "Поиск элемента списка (тип string)" << endl;
+	cout << setup << "" << endl;
+	a.find_element("aaa");
+	a.find_element("hhh");
+	a.find_element("eee");
+	a.find_element("dfs");
+	a.find_element("aaa8");
+	cout << endl << setup << "" << endl;
+	cout << "Удаление элементов списка (тип string)" << endl;
+	cout << setup << "" << endl;
+	a -= ("ddd");
+	a -= ("ddd");
+	a -= ("aaa");
+	a -= ("aaa");
+	a -= ("hhh");
+	a -= ("hhh");
+	a -= ("hhh");
+	a -= ("hhh");
+	a -= ("hhh");
+	cout << endl << setup << "" << endl;
+	cout << "Вывод списка после удаления элементов (тип string)\n";
+	cout << setup << "" << endl;
+	cout << a << endl;
+	a.delete_list();
+	cout << setup << "" << endl;
+	cout << "Вывод списка после удаления целиком (тип string)\n";
+	cout << setup << "" << endl;
+	cout << a << endl;
+
+	// Работа с шаблонным списком (тип int)
+	List<int> i(2);
+	i += (888);
+	i += (7);
+	i += (42);
+	i += (8);
+	i += (-111);
+	i += (11);
+	cout << setup << "" << endl;
+	cout << "Вывод списка (тип int)\n";
+	cout << setup << "" << endl;
+	i.print_list();
+	cout << endl << setup << "" << endl;
+	cout << "Поиск элемента списка (тип int)" << endl;
+	cout << setup << "" << endl;
+	i.find_element(8);
+	i.find_element(888);
+	i.find_element(7);
+	i.find_element(0);
+	i.find_element(100);
+	cout << endl << setup << "" << endl;
+	cout << "Удаление элементов списка (тип int)" << endl;
+	cout << setup << "" << endl;
+	i -= (7);
+	i -= (2);
+	i -= (11);
+	i -= (-111);
+	i -= (1);
+	cout << endl << setup << "" << endl;
+	cout << "Вывод списка после удаления элементов (тип int)\n";
+	cout << setup << "" << endl;
+	cout << i << endl;
+	cout << setup << "" << endl;
+	cout << "Вывод списка после удаления целиком (тип int)\n";
+	cout << setup << "" << endl;
+	i.delete_list();
+	i.print_list();
+
+	// Работа с STL списком
+	list<string> listSTL;
+	cout << endl << setup << "" << endl;
+	cout << "Вывод STL-списка\n";
+	cout << setup << "" << endl;
+	read_from_file(data, listSTL);
+	listSTL.sort();
+	printListSTL(listSTL);
+	cout << endl << setup << "" << endl;
+	cout << "Вывод STL-списка после удаления элементов\n";
+	cout << setup << "" << endl;
+	listSTL.remove("ddd");
+	listSTL.remove("aaa");
+	listSTL.remove("hhh");
+	printListSTL(listSTL);
+	cout << endl << setup << "" << endl;
+	cout << "Вывод STL-списка после удаления целиком\n";
+	cout << setup << "" << endl;
+	listSTL.clear();
+	printListSTL(listSTL);
+	if (!listSTL.size())
+	{
+		cout << "STL-список пуст!" << endl;
+	}
+
+	
+	return 0;
+}
